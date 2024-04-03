7cf9e56d8be0fdc3f46a645f323908c6481912dd
Joseph Watzson <49710538+joe-watson-sbf@users.noreply.github.com>
2024-04-02 23:29:03 -0500

    first commit

--- lib/config/firebase.ts
+++ lib/config/firebase.ts
@@ -1,20 +1,15 @@
-import type { ServiceAccount } from "firebase-admin";
-import { initializeApp, cert, getApps } from "firebase-admin/app";
+import { initializeApp } from "firebase/app";
+import { getStorage } from "firebase/storage";
 
-const activeApps = getApps();
-const serviceAccount = {
-  type: "service_account",
-  project_id: import.meta.env.FIREBASE_PROJECT_ID,
-  private_key_id: import.meta.env.FIREBASE_PRIVATE_KEY_ID,
-  private_key: import.meta.env.FIREBASE_PRIVATE_KEY,
-  client_email: import.meta.env.FIREBASE_CLIENT_EMAIL,
-  client_id: import.meta.env.FIREBASE_CLIENT_ID,
-  auth_uri: import.meta.env.FIREBASE_AUTH_URI,
-  token_uri: import.meta.env.FIREBASE_TOKEN_URI,
-  auth_provider_x509_cert_url: import.meta.env.FIREBASE_AUTH_CERT_URL,
-  client_x509_cert_url: import.meta.env.FIREBASE_CLIENT_CERT_URL,
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_apiKey,
+  authDomain: process.env.NEXT_PUBLIC_authDomain,
+  projectId: process.env.NEXT_PUBLIC_projectId,
+  storageBucket: process.env.NEXT_PUBLIC_storageBucket,
+  messagingSenderId: process.env.NEXT_PUBLIC_senderId,
+  appId: process.env.NEXT_PUBLIC_appId
 };
 
-export const app = activeApps.length === 0 ? initializeApp({
-  credential: cert(serviceAccount as ServiceAccount),
-}) : activeApps[0];
+
+export const app = initializeApp(firebaseConfig);
+export const storage = getStorage(app)
